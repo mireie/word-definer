3c8bf1e6c2101328dac69fc09281de2bdb1aaab7
mireie <michael.reiersgaard@gmail.com>
2021-07-23 09:53:53 -0700

    add home route

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
-require('sinatra')
-require('sinatra/reloader')
-require('./lib/word')
-require('./lib/definition')
-require('pry')
-also_reload('lib/**/*.rb')
+require("sinatra")
+require("sinatra/reloader")
+require("./lib/word")
+require("./lib/definition")
+require("pry")
+also_reload("lib/**/*.rb")
 
-get('/') do
-
+get("/") do
+  @words = Word.all
+  erb(:words)
 end
