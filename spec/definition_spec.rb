5c7e0d01a7be0faed25f2802093355e428d2d889
mireie <michael.reiersgaard@gmail.com>
2021-07-23 09:21:34 -0700

    update definition tests to include word ID

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -13,8 +13,8 @@ describe("#Definition") do
 
   describe("#==") do
     it("is the same definition if it has the same attributes as another definition") do
-      definition = Definition.new(nil, "Firey Beast")
-      definition2 = Definition.new(nil, "Firey Beast")
+      definition = Definition.new(nil, "Firey Beast", @word.id)
+      definition2 = Definition.new(nil, "Firey Beast", @word.id)
       definition.save
       definition2.save
       expect(definition).to(eq(definition2))
@@ -23,7 +23,7 @@ describe("#Definition") do
 
   describe("#save") do
     it("saves a definition") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
       expect(Definition.all[0].definition).to(eq("Hippopotamus"))
     end
@@ -31,7 +31,7 @@ describe("#Definition") do
 
   describe(".clear") do
     it("clears all saved definitions") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
       Definition.clear()
       expect(Definition.all).to(eq([]))
@@ -40,9 +40,9 @@ describe("#Definition") do
 
   describe(".find") do
     it("returns a definition by ID") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
-      definition2 = Definition.new(nil, "Geoff")
+      definition2 = Definition.new(nil, "Another cool thing.", @word2.id)
       definition2.save
       expect(Definition.find(definition2.id)).to(eq(definition2))
     end
@@ -50,16 +50,16 @@ describe("#Definition") do
 
   describe(".all") do
     it("returns an array of all definitions") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
-      definition2 = Definition.new(nil, "Geoff")
+      definition2 = Definition.new(nil, "Another cool thing.", @word2.id)
       definition2.save
       expect(Definition.all).to(eq([definition, definition2]))
     end
     it("returns an empty array if no definitions") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
-      definition2 = Definition.new(nil, "Geoff")
+      definition2 = Definition.new(nil, "Another cool thing.", @word2.id)
       definition2.save
       Definition.clear()
       expect(Definition.all).to(eq([]))
@@ -68,9 +68,9 @@ describe("#Definition") do
 
   describe(".delete") do
     it("deletes a definition") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
-      definition2 = Definition.new(nil, "Geoff")
+      definition2 = Definition.new(nil, "Another cool thing.", @word2.id)
       definition2.save
       definition.delete
       expect(Definition.all).to(eq([definition2]))
@@ -79,19 +79,11 @@ describe("#Definition") do
 
   describe(".update") do
     it("updates a definition") do
-      definition = Definition.new(nil, "Hippopotamus")
+      definition = Definition.new(nil, "A cool thing", @word.id)
       definition.save
-      definition.update("Frank")
-      expect(definition.definition).to(eq("Frank"))
+      definition.update("An uncool thing")
+      expect(definition.definition).to(eq("An uncool thing"))
     end
   end
 
-  describe(".update") do
-    it("updates a definition") do
-      definition = Definition.new(nil, "Hippopotamus")
-      definition.save
-      definition.update("Frank")
-      expect(definition.definition).to(eq("Frank"))
-    end
-  end
 end
