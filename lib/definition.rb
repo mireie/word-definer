7ab2b2749e26d6749ab88e6542f38197f1b57ec7
mireie <michael.reiersgaard@gmail.com>
2021-07-23 09:05:29 -0700

    duplicate class for Definition

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,10 +1,39 @@
 class Definition
-  attr_reader :id
+  attr_reader :id, :definition
   @@definitions = {}
   @@total_rows = 0
 
-  def initialize(id)
-    @id = id || @@total_rows += 1 
+  def initialize(id, definition)
+    @id = id || @@total_rows += 1
+    @definition = definition
+  end
 
+  def self.all
+    @@definitions.values
+  end
+
+  def save
+    @@definitions[self.id] = Definition.new(self.id, self.definition)
+  end
+
+  def self.clear
+    @@definitions = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
+
+  def ==(definition_to_compare)
+    self.definition() == definition_to_compare.definition()
+  end
+
+  def delete
+    @@definitions.delete(self.id)
+  end
+
+  def update(definition)
+    @definition = definition
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,8 +1,74 @@
 require "rspec"
 require "definition"
+require "pry"
 
-# describe("") do
-#   it("") do
-#     expect().to(eq())
-#   end
-# end
+describe("#Definition") do
+  before(:each) do
+    Definition.clear()
+  end
+
+  describe("#save") do
+    it("saves a definition") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      expect(Definition.all[0].definition).to(eq("Hippopotamus"))
+    end
+  end
+
+  describe(".clear") do
+    it("clears all saved definitions") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe(".find") do
+    it("returns a definition by ID") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      definition2 = Definition.new(nil, "Geoff")
+      definition2.save
+      expect(Definition.find(definition2.id)).to(eq(definition2))
+    end
+  end
+
+  describe(".all") do
+    it("returns an array of all definitions") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      definition2 = Definition.new(nil, "Geoff")
+      definition2.save
+      expect(Definition.all).to(eq([definition, definition2]))
+    end
+    it("returns an empty array if no definitions") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      definition2 = Definition.new(nil, "Geoff")
+      definition2.save
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe(".delete") do
+    it("deletes a definition") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      definition2 = Definition.new(nil, "Geoff")
+      definition2.save
+      definition.delete
+      expect(Definition.all).to(eq([definition2]))
+    end
+  end
+
+  describe(".update") do
+    it("updates a definition") do
+      definition = Definition.new(nil, "Hippopotamus")
+      definition.save
+      definition.update("Frank")
+      expect(definition.definition).to(eq("Frank"))
+    end
+  end
+end
